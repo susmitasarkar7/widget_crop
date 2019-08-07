1b70e804d89c69b46df24243c586f41c186496a9
Susmita Sarkar <sarkarsusmita253@gmail,com>
2019-08-07 22:24:22 +0530

    Form is processing

--- includes/functions.php
+++ includes/functions.php
@@ -1,5 +1,17 @@
 <?php
 
+    function redirect_to($new_location) {
+        header("Location: " . $new_location);
+        exit;
+    }
+
+    function mysql_prep($string) {
+        global $connection;
+
+        $escaped_string = mysqli_real_escape_string($connection, $string);
+        return $escaped_string;
+    }
+
     function confirm_query($result_set) {
         if(!$result_set) {
             die("Database query failed.");
